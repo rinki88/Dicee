d0c5cb86724cdcfc9e5dca4174f22d78d7c24661
rinki88 <70624844+rinki88@users.noreply.github.com>
2023-01-07 12:27:00 +0100

    Change src attribute

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "dice" + randomNumber1 + ".png";
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
 
@@ -13,7 +11,7 @@ image1.setAttribute("src", randomImageSource);*/
 
 var randomNumber2 =Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
